83afc8be34a66b51091bc806ab31fc2cef884dd7
katayama <yasucom4646@gmail.com>
2018-09-13 08:24:35 -0700

    change to variable

---
+++ app/src/main/java/com/yasuhiro/ca/find/entity/const.kt
@@ -0,0 +1,14 @@
+package com.yasuhiro.ca.find.entity
+
+import java.io.Serializable
+
+class const {
+    companion object {
+        const val UsersPath: String = "Users"
+        const val ContentsPath: String = "Contents"
+        const val MeassagesPath: String = "Messages"
+        const val PlacesPath: String = "Places"
+        const val RestaurantsPath: String = "Restaurants"
+        const val HousesPath: String = "Houses"
+    }
+}
