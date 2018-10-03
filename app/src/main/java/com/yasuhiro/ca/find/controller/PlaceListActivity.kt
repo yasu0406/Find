bdee4de7cd493df22611397a539aef75e0229047
katayama <yasucom4646@gmail.com>
2018-10-03 14:46:13 -0700

    add comment and change firebasedb path of place

--- app/src/main/java/com/yasuhiro/ca/find/controller/PlaceListActivity.kt
+++ app/src/main/java/com/yasuhiro/ca/find/controller/PlaceListActivity.kt
@@ -7,22 +7,33 @@ import android.widget.ListView
 import com.google.firebase.database.*
 import com.yasuhiro.ca.find.R
 import com.yasuhiro.ca.find.adapter.PlacesAdapter
-import com.yasuhiro.ca.find.entity.const.Companion.PlacesPath
+import com.yasuhiro.ca.find.entity.Const.Companion.PLACE_DBPATH
 import com.yasuhiro.ca.find.model.Place
 
 import kotlinx.android.synthetic.main.activity_place_list.*
 
+
+/*
+ *
+ * ClassName:PlaceListActivity
+ * Date:2018/09/10
+ * Create by: Yasuhiro Katayama
+ *
+ */
 class PlaceListActivity : AppCompatActivity() {
 
+    // variables of Firebase
     private var mDatabaseReference: DatabaseReference? = null
     private var mListView: ListView? = null
     private var listPlaceArrayList: ArrayList<Place>? = null
     private var placeAddapter: PlacesAdapter? = null
 
+    // variables of data
     private var uid: String? = null
     private var placeName: String? = null
     private var discription: String? = null
     private var address: String? = null
+    private var image: String? = null
     private var placeMap: MutableMap<String, Any>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,17 +45,29 @@ class PlaceListActivity : AppCompatActivity() {
             var intent = Intent(this,RegistPlaceActivity::class.java)
             startActivity(intent)
         }
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference(PlacesPath)
+        // call FirebaseDatabase
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference(PLACE_DBPATH)
+        // call listView
         mListView = findViewById(R.id.listPlacesView) as ListView
+        // set ArrayList<Place>
         listPlaceArrayList = ArrayList<Place>()
+        // set PlacesAdapter(this)
         placeAddapter = PlacesAdapter(this)
+        // clear listPlaceArrayList
         listPlaceArrayList!!.clear()
+        // set listPlaceArrayList for placeAddapter
         placeAddapter!!.setlistArray(listPlaceArrayList)
+        // set placeAddapter for mListView
         mListView!!.setAdapter(placeAddapter)
+        // call placeList fun
         placeList()
 
     }
 
+    /*
+    *
+    *
+    * */
     private fun placeList() {
         var mChildEventListener =
         object : ChildEventListener {
@@ -54,6 +77,7 @@ class PlaceListActivity : AppCompatActivity() {
                 placeName = placeMap!!.get("placeName") as String
                 discription = placeMap!!.get("discription") as String
                 address = placeMap!!.get("address") as String
+
 
                 var place = Place(placeName, discription, address)
                 listPlaceArrayList!!.add(place)
