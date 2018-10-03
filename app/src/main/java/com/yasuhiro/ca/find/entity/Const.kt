57d1750e9299b898dded8c4f745f31e90981d951
katayama <yasucom4646@gmail.com>
2018-10-03 14:37:17 -0700

    add variables and rename

--- app/src/main/java/com/yasuhiro/ca/find/entity/Const.kt
+++ app/src/main/java/com/yasuhiro/ca/find/entity/Const.kt
@@ -2,11 +2,14 @@ package com.yasuhiro.ca.find.entity
 
 class Const {
     companion object {
-        const val UsersPath: String = "Users"
-        const val ContentsPath: String = "Contents"
-        const val MeassagesPath: String = "Messages"
-        const val PlacesPath: String = "Places"
-        const val RestaurantsPath: String = "Restaurants"
-        const val HousesPath: String = "Houses"
+        const val USER_DBPATH: String = "Users"
+        const val CONTENT_DBPATH: String = "Contents"
+        const val MESSAGE_DBPATH: String = "Messages"
+        const val PLACE_DBPATH: String = "Places"
+        const val RESTAURANT_DBPATH: String = "Restaurants"
+        const val HOUSE_DBPATH: String = "Houses"
+        const val PLACE_STPATH: String = "PlacesImg"
+        const val RESTAURANT_STPATH: String = "RestaurantsImg"
+        const val HOUSE_STPATH: String = "HousesImg"
     }
 }
