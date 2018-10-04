391657d37dce8682a87dec64a6e687042887dd3d
katayama <yasucom4646@gmail.com>
2018-10-04 13:38:13 -0700

    delete toMap() and add variable

--- app/src/main/java/com/yasuhiro/ca/find/model/Place.kt
+++ app/src/main/java/com/yasuhiro/ca/find/model/Place.kt
@@ -1,15 +1,4 @@
 package com.yasuhiro.ca.find.model
 
-import com.google.firebase.database.Exclude
 
-data class Place(val placeName: String = "", val discription: String = "", val address: String?) {
-    // [START post_to_map]
-    @Exclude
-    fun toMap(): Map<String, Any?> {
-        return mapOf(
-                "placeName" to placeName,
-                "discription" to discription,
-                "starCount" to address
-        )
-    }
-}
+data class Place(val placeName: String?, val discription: String?, val address: String?, val imageUrl: String?)
