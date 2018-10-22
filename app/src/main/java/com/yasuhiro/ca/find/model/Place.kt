069dcb443b143694754a5d18cf87abefe7712755
katayama <yasucom4646@gmail.com>
2018-10-21 22:46:06 -0700

    remake variables

--- app/src/main/java/com/yasuhiro/ca/find/model/Place.kt
+++ app/src/main/java/com/yasuhiro/ca/find/model/Place.kt
@@ -1,4 +1,4 @@
 package com.yasuhiro.ca.find.model
 
 
-data class Place(val placeName: String?, val discription: String?, val address: String?, val imageUrl: String?)
+data class Place(val placeId: String?, val placeName: String?, val discription: String?, val address: String?, val imageUrl: String?)
--- app/src/main/java/com/yasuhiro/ca/find/model/User.kt
+++ app/src/main/java/com/yasuhiro/ca/find/model/User.kt
@@ -1,3 +1,3 @@
 package com.yasuhiro.ca.find.model
 
-data class User (val uid: String = "", val userName: String, val email: String, val password: String)
+data class User (val uid: String?, val userName: String?, val email: String?, val password: String?)
