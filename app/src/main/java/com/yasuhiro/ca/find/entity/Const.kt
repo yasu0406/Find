b7716eb10f08bae40b0fbe83ff2e079826ce0c92
katayama <yasucom4646@gmail.com>
2018-10-21 22:45:33 -0700

    add REVIEW_DBPATH of variable

--- app/src/main/java/com/yasuhiro/ca/find/entity/Const.kt
+++ app/src/main/java/com/yasuhiro/ca/find/entity/Const.kt
@@ -6,6 +6,7 @@ class Const {
         const val CONTENT_DBPATH: String = "Contents"
         const val MESSAGE_DBPATH: String = "Messages"
         const val PLACE_DBPATH: String = "Places"
+        const val REVIEW_DBPATH: String = "Reviews"
         const val RESTAURANT_DBPATH: String = "Restaurants"
         const val HOUSE_DBPATH: String = "Houses"
         const val PLACE_STPATH: String = "PlacesImg"
