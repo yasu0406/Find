5a2e8160536f569cd1d6e2b699421d0a0f7e275f
katayama <yasucom4646@gmail.com>
2018-10-24 21:49:51 -0700

    add comment and variable

--- app/src/main/java/com/yasuhiro/ca/find/controller/PlaceDetailActivity.kt
+++ app/src/main/java/com/yasuhiro/ca/find/controller/PlaceDetailActivity.kt
@@ -2,36 +2,71 @@ package com.yasuhiro.ca.find.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.yasuhiro.ca.find.R
+import com.yasuhiro.ca.find.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.content_place_detail.*
+import kotlinx.android.synthetic.main.toolbar_backbutton.*
 
-class PlaceDetailActivity : AppCompatActivity() {
+/*
+ *
+ * ClassName:PlaceDetailActivity
+ * Date:2018/10/15
+ * Create by: Yasuhiro Katayama
+ *
+ */
+class PlaceDetailActivity : AppCompatActivity(), View.OnClickListener {
 
     private var placeImage: ImageView? = null
     private var title: TextView? = null
-    private var discription: TextView? = null
+    private lateinit var viewpageradapter: ViewPagerAdapter
+
+    private var retunButton: TextView? = null
+    private var placeId: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_detail)
+        setSupportActionBar(toolbar)
+        viewpageradapter = ViewPagerAdapter(supportFragmentManager)
+
+        this.viewPager.adapter = viewpageradapter
+        this.tab_layout.setupWithViewPager(this.viewPager)
 
         val extras = intent.extras
 
         var placeName = extras.get("placeName")
-        var placeDiscription = extras.get("discription")
         var placeImageUrl = extras.get("placeImageUrl")
+        placeId = extras.getString("placeId")
 
         placeImage = findViewById(R.id.placeImage)
         title =  findViewById(R.id.title)
-        discription = findViewById(R.id.discription)
 
         Glide.with(placeImage)
                 .load(placeImageUrl)
                 .into(placeImage)
         title!!.setText(placeName as String)
-        discription!!.setText(placeDiscription as String)
 
+        retunButton = findViewById(R.id.backButton)
+        retunButton!!.setOnClickListener(this)
+
+    }
+
+    // returnButton function
+    private fun returnButton() {
+        super.onBackPressed()
+    }
+
+    // onClick function
+    override fun onClick(v: View) {
+        when(v.id) {
+
+            R.id.backButton -> returnButton()
+
+        }
     }
 }
