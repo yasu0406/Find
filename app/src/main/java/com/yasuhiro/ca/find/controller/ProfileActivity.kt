6704d55b2da2981ae8956c75bf27368bd28bbd14
katayama <yasucom4646@gmail.com>
2018-11-19 08:00:19 -0800

    Created ProfileActivity

---
+++ app/src/main/java/com/yasuhiro/ca/find/controller/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.yasuhiro.ca.find.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.yasuhiro.ca.find.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
